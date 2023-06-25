647a934d2666571c48e1eb7020fde37503cb51b8
smellycat5 <prajapatichunan@gmail.com>
2023-06-25 18:54:25 +0545

    added settings

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     use HasFactory;
+
+    public function admin_can_add_setting()
+    {
+        
+    }
+    
+    public function admin_can_edit_setting()
+    {
+        
+    }
+
+    public function admin_can_delete_setting()
+    {
+        
+    }
 }
