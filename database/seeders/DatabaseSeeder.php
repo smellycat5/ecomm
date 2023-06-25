b2392d758aed15df13fb611e4e40afe68cb6c590
smellycat5 <prajapatichunan@gmail.com>
2023-06-25 23:59:31 +0545

    minor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Chunan Prajapati',
+            'name' => 'Smellycat user',
             'email' => 'test@test.com',
         'password' => 'test123',
         ]);
